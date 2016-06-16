7988fc4a66ccdad8f7353c02dc60f541b5ca88b2
Florian Grandjean <florian.pdf@gmail.com>
2016-06-16 23:58:39 +0200

    modif rout

--- src/UserBundle/Resources/public/js/services/userService.js
+++ src/UserBundle/Resources/public/js/services/userService.js
@@ -2,17 +2,17 @@
 function userService($http) {
     return {
         get : function() {
-            return $http.get('api/user/users.json');
+            return $http.get('app_dev.php/api/user/users.json');
         },
         update : function(id, data){
             delete data.id;
-            return $http.put('api/user/edit/' + id + '.json', data);
+            return $http.put('app_dev.php/api/user/edit/' + id + '.json', data);
         },
         create : function(data) {
-            return $http.post('api/user/new.json', data);
+            return $http.post('app_dev.php/api/user/new.json', data);
         },
         delete : function(id) {
-            return $http.delete('api/user/delete/' + id + '.json');
+            return $http.delete('app_dev.php/api/user/delete/' + id + '.json');
         }
     }
 };
